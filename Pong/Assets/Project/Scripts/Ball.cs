aabb68a56f3266d1b1604037f28bb4f7003232fa
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-03-11 21:05:12 -0500

    Add a 2nd paddle; Implement Increasing Difficulty; Add sound; Complete Game!

--- Pong/Assets/Project/Scripts/Ball.cs
+++ Pong/Assets/Project/Scripts/Ball.cs
@@ -8,6 +8,8 @@ public class Ball : MonoBehaviour
     public float MaxXSpeed = 1.2f;
     public float MinYSpeed = 0.8f;
     public float MaxYSpeed = 1.2f;
+    // 10% difficulty increase
+    public float DifficultyMultiplier = 1.1f;
 
     private Rigidbody2D ballRigidBody;
     // Start is called before the first frame update
@@ -17,7 +19,7 @@ public class Ball : MonoBehaviour
         // Set the starting velocity to a random number between our defined ranges
         // Random.value returns a float between 0 and 1, so if its below 0.5 make it go down, otherwise go up
         ballRigidBody.velocity = new Vector2(Random.Range(MinXSpeed, MaxXSpeed) * (Random.value > 0.5f ? -1 : 1), 
-                                                                      Random.Range(MinYSpeed, MaxYSpeed) * (Random.value > 0.5f ? -1 : 1));
+                                             Random.Range(MinYSpeed, MaxYSpeed) * (Random.value > 0.5f ? -1 : 1));
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class Ball : MonoBehaviour
     // This doesn't have to be called in another method because it calls itself when it enters a collider... Hence the name ; )
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GetComponent<AudioSource>( ).Play( );
         if (collision.tag == "Limit")
         {
             // If the position of the limit on the y axis is greater than the ball's y axis position we know its the top limit
@@ -47,11 +50,11 @@ public class Ball : MonoBehaviour
         {
             if (collision.transform.position.x < transform.position.x && ballRigidBody.velocity.x < 0)
             {
-                ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x, ballRigidBody.velocity.y);
+                ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x * DifficultyMultiplier, ballRigidBody.velocity.y * DifficultyMultiplier);
             }
             if (collision.transform.position.x > transform.position.x && ballRigidBody.velocity.x > 0)
             {
-                ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x, ballRigidBody.velocity.y);
+                ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x * DifficultyMultiplier, ballRigidBody.velocity.y * DifficultyMultiplier);
             }
         }
     }
--- Pong/Assets/Project/Scripts/Paddle.cs
+++ Pong/Assets/Project/Scripts/Paddle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     public float Speed = 1f;
+    public int PlayerIndex = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float verticalMovement = Input.GetAxis("Vertical");
+        float verticalMovement = Input.GetAxis("Vertical" + PlayerIndex);
         GetComponent<Rigidbody2D>( ).velocity = new Vector2(0, verticalMovement * Speed);
     }
 }
