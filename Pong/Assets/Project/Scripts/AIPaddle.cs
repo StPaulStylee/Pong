d736eb637be4d71458c66f8aa3b8f7ad96ea1105
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-03-16 06:31:59 -0500

    Implement AI that is beatable; editable difficulty

--- Pong/Assets/Project/Scripts/AIPaddle.cs
+++ Pong/Assets/Project/Scripts/AIPaddle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AIPaddle : MonoBehaviour
 {
+    public float Speed = 1f;
     public GameObject currentBall;
     private Vector2 startPosition;
     // Start is called before the first frame update
@@ -18,9 +19,21 @@ public class AIPaddle : MonoBehaviour
         currentBall = GameObject.FindWithTag("Ball");
         if (currentBall != null)
         {
+            Vector2 ballPosition = currentBall.transform.position;
+            Vector2 paddlePosition = this.transform.position;
+            if (paddlePosition.y > ballPosition.y)
+            {
+                //paddleposition.y -= this.speed;
+                GetComponent<Rigidbody2D>( ).velocity = new Vector2(0, -1 * this.Speed);
+            }
+            else if (paddlePosition.y < ballPosition.y)
+            {
+                //paddlePosition.y += this.Speed;
+                GetComponent<Rigidbody2D>( ).velocity = new Vector2(0, 1 * this.Speed);
+            }
             // set the paddle location equal to the balls y location
-            GetComponent<Rigidbody2D>( ).position = new Vector2(startPosition.x, currentBall.transform.position.y);
-            Debug.Log(currentBall.transform.position.y);
+            //GetComponent<Rigidbody2D>( ).velocity = new Vector2(startPosition.x, paddlePosition.y);
+            //Debug.Log(currentBall.transform.position.y);
         }
     }
 }
