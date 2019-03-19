ce1f70cadf5ab1f207da0397d49aea791d020a54
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-03-19 06:26:19 -0500

    Implement smooth motion of AI Controller by using SmoothDampen method

--- Pong/Assets/Project/Scripts/AIPaddle.cs
+++ Pong/Assets/Project/Scripts/AIPaddle.cs
@@ -6,17 +6,18 @@ public class AIPaddle : MonoBehaviour
 {
     public float Speed = 1f;
     public GameObject currentBall;
+
+    private float SmoothTime = 0.01f;
     private Vector2 startPosition;
-    private Rigidbody2D paddle;
+    private Vector2 velocity = Vector2.zero;
     // Start is called before the first frame update
     void Start( )
     {
         startPosition = transform.position;
-        paddle = GetComponent<Rigidbody2D>( );
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (currentBall == null)
         {
@@ -28,23 +29,9 @@ public class AIPaddle : MonoBehaviour
         {
             Vector2 ballPosition = currentBall.transform.position;
             Vector2 paddlePosition = this.transform.position;
-            if (paddlePosition.y > ballPosition.y)
-            {
-                //paddleposition.y -= this.speed;
-                //paddle.velocity = new Vector2(0, -1 * this.Speed);
-                paddle.velocity = Vector2.Lerp(Vector2.zero, Vector2.down * this.Speed, 0.5f);
-                //this.transform.position = new Vector2(startPosition.x, paddlePosition.y - (this.Speed * Time.deltaTime));
-            }
-            else if (paddlePosition.y < ballPosition.y)
-            {
-                //paddlePosition.y += this.Speed;
-                //paddle.velocity = new Vector2(0, 1 * this.Speed);
-                paddle.velocity = Vector2.Lerp(Vector2.zero, Vector2.up * this.Speed, 0.5f);
-                //this.transform.position = new Vector2(startPosition.x, paddlePosition.y + (this.Speed * Time.deltaTime));
-            }
-            // set the paddle location equal to the balls y location
-            //GetComponent<Rigidbody2D>( ).velocity = new Vector2(startPosition.x, paddlePosition.y);
-            //Debug.Log(paddle.velocity);
+            Vector2 targetPosition = new Vector2(startPosition.x, ballPosition.y);
+            this.transform.position = Vector2.SmoothDamp(this.transform.position, targetPosition, 
+                                                         ref velocity, SmoothTime, Speed);
         }
     }
 }
